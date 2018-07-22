08ffcc97b7cc2c5787b5c2c68de10462785a463a
tsompos <jtsompos@gmail.com>
2018-07-22 18:22:03 +0300

    Spring Boot initialization

---
+++ src/main/java/com/trials/statsy/StatsyApplication.java
@@ -0,0 +1,12 @@
+package com.trials.statsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatsyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/trials/statsy/StatsyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trials.statsy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StatsyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
